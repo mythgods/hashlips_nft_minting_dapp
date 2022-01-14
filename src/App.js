2a97f1e358c04e83488da2eb3d353fa3589da1eb
mythgods <mythgods99@gmail.com>
2022-01-14 05:38:01 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -205,7 +205,7 @@ function App() {
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg alt={"example"} src={"/config/images/example.gif"} />
+            <StyledImg alt={"example"} src={"/config/images/shark1.png"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
@@ -373,7 +373,7 @@ function App() {
           <s.Container flex={1} jc={"center"} ai={"center"}>
             <StyledImg
               alt={"example"}
-              src={"/config/images/example.gif"}
+              src={"/config/images/shark2.png"}
               style={{ transform: "scaleX(-1)" }}
             />
           </s.Container>
